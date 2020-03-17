087d89e99bcaa574851548823b11d9e4e4077888
nicolasmartini <32593875+nicolasmartini@users.noreply.github.com>
2020-03-16 22:33:20 -0300

    Se agregan comentarios e indenta codigo

--- src/challenge_mp/Login.java
+++ src/challenge_mp/Login.java
@@ -3,17 +3,14 @@ package challenge_mp;
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.mail.MessagingException;
 import javax.mail.NoSuchProviderException;
 import javax.mail.Session;
 import javax.mail.Store;
-import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
-
 import java.awt.FlowLayout;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
@@ -24,12 +21,14 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Properties;
-
 import javax.swing.JButton;
 import java.awt.Color;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 /**
- * Clase Login 
+ * Clase para loguearse a la cuenta de Gmail y procesar los correos
+ * 
  * @author N1C0
  *
  */
@@ -38,68 +37,86 @@ public class Login extends JFrame {
 	private JTextField txtPass;
 	private Login context;
 	private Store store;
-	
+	private JButton btnNewButton;
+
 	public Login() {
-		
+
 		context = this;
-		
-	PanelImagen panel_login = new PanelImagen("./src/imagenes/fondo_login.jpg");			    
+
+		PanelImagen panel_login = new PanelImagen("./src/imagenes/fondo_login.jpg");
 		getContentPane().add(panel_login, BorderLayout.CENTER);
 		panel_login.setLayout(new BorderLayout(0, 20));
-		
+
 		JLabel lblNewLabel = new JLabel("Ingrese datos de su cuenta de Gmail");
 		lblNewLabel.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 40));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_login.add(lblNewLabel, BorderLayout.NORTH);
-		
+
 		JPanel panel = new JPanel();
 		panel_login.add(panel, BorderLayout.CENTER);
-		panel.setOpaque(false);		
+		panel.setOpaque(false);
 		panel.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel panel_1 = new JPanel();
 		FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
 		flowLayout.setVgap(50);
-		panel_1.setPreferredSize(new Dimension(panel_1.getWidth(),100));
-		panel_1.setOpaque(false);	
+		panel_1.setPreferredSize(new Dimension(panel_1.getWidth(), 100));
+		panel_1.setOpaque(false);
 		panel.add(panel_1, BorderLayout.NORTH);
-		
-		JLabel lblNewLabel_1 = new JLabel();			
+
+		JLabel lblNewLabel_1 = new JLabel();
 		ImageIcon ima = new ImageIcon("./src/imagenes/user.png");
 		Icon icon = new ImageIcon(ima.getImage().getScaledInstance(40, 40, Image.SCALE_AREA_AVERAGING));
 		lblNewLabel_1.setIcon(icon);
 		panel_1.add(lblNewLabel_1);
-		
-		
+
 		txtUser = new JTextField();
-		txtUser.setText("nicolasarielmartini@gmail.com");
+		txtUser.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+
+					txtPass.requestFocus();
+				}
+			}
+		});
+		txtUser.setText("");
 		panel_1.add(txtUser);
 		txtUser.setOpaque(false);
 		txtUser.setColumns(30);
-		
+
 		JPanel panel_2 = new JPanel();
-		panel_2.setOpaque(false);	
+		panel_2.setOpaque(false);
 		panel.add(panel_2, BorderLayout.CENTER);
-		
+
 		JLabel lblNewLabel_2 = new JLabel();
 		ImageIcon ima2 = new ImageIcon("./src/imagenes/lock.png");
 		Icon icon2 = new ImageIcon(ima2.getImage().getScaledInstance(40, 40, Image.SCALE_AREA_AVERAGING));
 		lblNewLabel_2.setIcon(icon2);
 		panel_2.add(lblNewLabel_2);
-		
+
 		txtPass = new JPasswordField();
 		panel_2.add(txtPass);
 		txtPass.setOpaque(false);
 		txtPass.setColumns(30);
-		
-		JPanel panel_3 = new JPanel();
-		FlowLayout flowLayout_1 = (FlowLayout) panel_3.getLayout();
+
+		txtPass.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+
+					btnNewButton.doClick();
+				}
+			}
+		});
+
+		JPanel panel_3 = new JPanel();		
 		panel_3.setForeground(Color.WHITE);
 		panel_3.setBackground(Color.WHITE);
 		panel_3.setOpaque(false);
 		panel.add(panel_3, BorderLayout.SOUTH);
-		
-		JButton btnNewButton = new JButton("");
+
+		btnNewButton = new JButton("");
 		btnNewButton.setForeground(Color.WHITE);
 		btnNewButton.setBackground(Color.WHITE);
 		ImageIcon ima3 = new ImageIcon("./src/imagenes/aceptar.png");
@@ -108,61 +125,58 @@ public class Login extends JFrame {
 		btnNewButton.setOpaque(false);
 		btnNewButton.setBorder(null);
 		panel_3.add(btnNewButton);
-		
+
 		btnNewButton.addActionListener(new ActionListener() {
-			
+
 			private Principal p;
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+
 				String msg = validar();
 				if (msg == null) {
-										
+
 					if (iniciar_seccion()) {
-						
-						p = new Principal(store,context);
-						
-						
-					}else {
-						
-						JOptionPane.showMessageDialog(context,"Error al inicar session", "Error",JOptionPane.ERROR_MESSAGE);
+
+						p = new Principal(store, context);
+
+					} else {
+
+						JOptionPane.showMessageDialog(context, "Error al inicar session", "Error",
+								JOptionPane.ERROR_MESSAGE);
 					}
-					
-				}else {
-					
-					JOptionPane.showMessageDialog(context,msg, "Error",JOptionPane.ERROR_MESSAGE);
+
+				} else {
+
+					JOptionPane.showMessageDialog(context, msg, "Error", JOptionPane.ERROR_MESSAGE);
 				}
-				
+
 			}
 
 			private String validar() {
-				
+
 				String user = txtUser.getText();
 				String pass = txtPass.getText();
-				
-				if (user== null || user.trim().equals("")) {
+
+				if (user == null || user.trim().equals("")) {
 					return "Debe ingresar un correo electrónico";
-					
+
 				}
-				
+
 				if (!user.contains("@gmail.com")) {
-					
-					return  "Debe ingresar un correo válido de Gmail";
+
+					return "Debe ingresar un correo válido de Gmail";
 				}
-				
+
 				if (pass == null || pass.trim().equals("")) {
-					
+
 					return "Debe ingresar una contraseña";
 				}
-				
-				
-				
+
 				return null;
 			}
 		});
-		
-		
+
 		JButton btnNewButton_1 = new JButton("");
 		btnNewButton_1.setForeground(Color.WHITE);
 		btnNewButton_1.setBackground(Color.WHITE);
@@ -172,66 +186,64 @@ public class Login extends JFrame {
 		btnNewButton_1.setIcon(icon4);
 		btnNewButton_1.setOpaque(false);
 		panel_3.add(btnNewButton_1);
-		
+
 		btnNewButton_1.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				context.dispose();
-				
+
 			}
 		});
-		
-		this.setSize(new Dimension(600,350));
+
+		this.setSize(new Dimension(600, 350));
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
 		this.setVisible(true);
 		this.repaint();
 
 	}
-	
-	 private boolean iniciar_seccion() {
-		// Se obtiene la Session
-       Properties prop = new Properties();
-       prop.setProperty("mail.pop3.starttls.enable", "false");
-       prop.setProperty("mail.pop3.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-       prop.setProperty("mail.pop3.socketFactory.fallback", "false");
-       prop.setProperty("mail.pop3.port", "995");
-       prop.setProperty("mail.pop3.socketFactory.port", "995");
-       Session sesion = Session.getInstance(prop);
-       
-       String user = txtUser.getText();
-	String pass = txtPass.getText();
-       // sesion.setDebug(true);
 
-     
-         // Se obtiene el Store y el Folder, para poder leer el
-         // correo.
-        
-			try {
-				store = sesion.getStore("pop3");
-				store.connect("pop.gmail.com", user, pass);
-				
-				System.out.println("Inicio de Seccion");
-				
-				return true;
-			} catch (NoSuchProviderException e) {
-				JOptionPane.showMessageDialog(this,e.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
-				
-			} catch (MessagingException e) {
-				JOptionPane.showMessageDialog(this,"Usuario y/o clave incorrecta","Error",JOptionPane.ERROR_MESSAGE);
-			}
-       
-		
-			return false;
+	/**
+	 *  Realiza la conexion API de  Gmail
+	 * @return true si se puedo realizar la conexion con la cuenta de Gmail 
+	 */
+	private boolean iniciar_seccion() {
+		// Se obtiene la Session
+		Properties prop = new Properties();
+		prop.setProperty("mail.pop3.starttls.enable", "false");
+		prop.setProperty("mail.pop3.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		prop.setProperty("mail.pop3.socketFactory.fallback", "false");
+		prop.setProperty("mail.pop3.port", "995");
+		prop.setProperty("mail.pop3.socketFactory.port", "995");
+		Session sesion = Session.getInstance(prop);
+
+		String user = txtUser.getText();
+		String pass = txtPass.getText();
+		// sesion.setDebug(true);
+
+		// Se obtiene el Store y el Folder, para poder leer los correos
+
+		try {
+			store = sesion.getStore("pop3");
+			store.connect("pop.gmail.com", user, pass);
+
+			System.out.println("Inicio de Seccion");
+
+			return true;
+		} catch (NoSuchProviderException e) {
+			JOptionPane.showMessageDialog(this, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+
+		} catch (MessagingException e) {
+			JOptionPane.showMessageDialog(this, "Usuario y/o clave incorrecta", "Error", JOptionPane.ERROR_MESSAGE);
+		}
+
+		return false;
 	}
-	
-	
-	  public static void main(String[] args)
-	    {
-		  
-		  Login l =  new Login();
-		  
-		  
-	    }
+
+	public static void main(String[] args) {
+
+		Login l = new Login();
+
+	}
 }
--- src/challenge_mp/Principal.java
+++ src/challenge_mp/Principal.java
@@ -10,305 +10,161 @@ import javax.mail.MessagingException;
 import javax.mail.Multipart;
 import javax.mail.Part;
 import javax.mail.Store;
-import javax.management.monitor.Monitor;
+import javax.swing.JOptionPane;
 
-
+/**
+ * Clase encargada de procesar los correos electronicos y guardarlos en la base
+ * de datos
+ * 
+ * @author N1C0
+ *
+ */
 public class Principal {
-	
 
 	private Store store;
 	private String key = "DevOps";
 	private Operaciones op;
 	private Login login;
-	
-	public Principal (Store store,Login l) {
-		
+
+	public Principal(Store store, Login l) {
+
 		this.store = store;
 		this.login = l;
 		conectar_db();
-		
-		if (op!= null) {
+
+		if (op != null) {
 			procesar_correos();
 		}
 	}
-	
-	 private void conectar_db() {
-		 ConexionMySQL c = new ConexionMySQL();
-	     if (c.Conectar("admin", "admin")!= null){
-	    	 System.out.println("Acceso a la bd");
-	    	 op = new Operaciones(c.getConnection());
-	    	 
-	     }
-		
+
+	/*
+	 * Se crea la conexion a la base de datos
+	 */
+	private void conectar_db() {
+		ConexionMySQL c = new ConexionMySQL();
+		if (c.Conectar("admin", "admin") != null) {
+			System.out.println("Acceso a la bd");
+			op = new Operaciones(c.getConnection());
+
+		}
+
 	}
 
 	public void setStore(Store store) {
-		 
-		 this.store = store;
-		 
-	 }
 
+		this.store = store;
 
+	}
+
+	/**
+	 * Se procesan todos los correos encontrados y si contiene la palabra clave se guardan en la bd
+	 */
 	private void procesar_correos() {
+
+		int res = JOptionPane.showConfirmDialog(login, "Desea procesar los correos", "Confirmar",
+				JOptionPane.YES_NO_OPTION);
+
+		if (res == JOptionPane.NO_OPTION) {
+
+			return;
+
+		}
+
+		login.setCursor(Cursor.getDefaultCursor().WAIT_CURSOR);
+
 		Folder folder;
 		try {
+
 			folder = store.getFolder("INBOX");
 			folder.open(Folder.READ_ONLY);
-			
-            // Se obtienen los mensajes.
-            Message[] mensajes = folder.getMessages();
 
-            String from;
-            String subject;
-            Date date;
-            String fecha;
-          
-            login.setCursor(Cursor.getDefaultCursor().WAIT_CURSOR);
+			// Se obtienen los mensajes.
+			Message[] mensajes = folder.getMessages();
 
-            for (int i = 0; i < mensajes.length; i++)
-            {
-            	subject = mensajes[i].getSubject();
-            	System.out.println(subject);
-            	if (subject.contains(key) || chequear_partes(mensajes[i])) {
-            		
-            		from = mensajes[i].getFrom()[0].toString();
-            		if (from.contains("<") && from.contains(">")) {
-            			from = (String) from.subSequence(from.indexOf("<")+1,from.indexOf(">"));
-            		}
-            		date = mensajes[i].getSentDate();
-            		SimpleDateFormat formatofecha = new SimpleDateFormat("yyyy-MM-dd");
-            		fecha=formatofecha.format(date);    
-            		
-            		System.out.println(fecha);
-            		System.out.println(from);
-            		
-            		
-            		guardar_registro(fecha,from,subject);
-            		
-            	}                
-          
-            }
-            
-            login.setCursor(Cursor.getDefaultCursor());
+			System.out.println("mensajes " + mensajes.length);
 
-            folder.close(false);
-            store.close();
-			
-			
+			String from;
+			String subject;
+			Date date;
+			String fecha;
+
+			// Por cada mensaje se verifica si contiene en su asuento o cuerpo la palabra
+			// buscada (DevOps)
+			for (int i = 0; i < mensajes.length; i++) {
+				subject = mensajes[i].getSubject();
+				System.out.println(subject);
+				if (subject.contains(key) || chequear_partes(mensajes[i])) {
+
+					from = mensajes[i].getFrom()[0].toString();
+					if (from.contains("<") && from.contains(">")) {
+						from = (String) from.subSequence(from.indexOf("<") + 1, from.indexOf(">"));
+					}
+					date = mensajes[i].getSentDate();
+					SimpleDateFormat formatofecha = new SimpleDateFormat("yyyy-MM-dd");
+					fecha = formatofecha.format(date);
+					guardar_registro(fecha, from, subject);
+
+				}
+
+			}
+
+			login.setCursor(Cursor.getDefaultCursor());
+
+			folder.close(false);
+			store.close();
+
+			login.setVisible(true);
+
 		} catch (MessagingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-       
-		
+
 	}
 
+	/**
+	 * Guardar en la bd informacion sobre un correo
+	 * 
+	 * @param fecha fecha de recepcion del correo
+	 * @param from remitente del correo
+	 * @param subject asutno del correo
+	 */
 	private void guardar_registro(String fecha, String from, String subject) {
-		
-			op.guardar_registro(fecha, from, subject);
+
+		op.guardar_registro(fecha, from, subject);
 	}
 
 	private boolean chequear_partes(Part parte) {
-        // Si es multipart, se analiza cada una de sus partes recursivamente.
-        try {
+
+		try {
 			if (parte.isMimeType("multipart/*")) {
-			
-			    Multipart multi;
-			    multi = (Multipart) parte.getContent();
-			    
-			    for (int j = 0; j < multi.getCount(); j++)
-			    {
-			    	
-			    	if (multi.getBodyPart(j).isMimeType("text/*")) {
-			    		
-			    	 	if (parte.getContent().toString().contains(key)){
-		               		return true;
-		               	}
-			    		
-			    	}
-			    }
-			}else if (parte.isMimeType("text/*")) {            
-               
-               	if (parte.getContent().toString().contains(key)){
-               		return true;
-               	}
-                
-            }
+
+				Multipart multi;
+				multi = (Multipart) parte.getContent();
+
+				for (int j = 0; j < multi.getCount(); j++) {
+
+					if (multi.getBodyPart(j).isMimeType("text/*")) {
+
+						if (parte.getContent().toString().contains(key)) {
+							return true;
+						}
+
+					}
+				}
+			} else if (parte.isMimeType("text/*")) {
+
+				if (parte.getContent().toString().contains(key)) {
+					return true;
+				}
+
+			}
 		} catch (MessagingException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
-        return false;
+
+		return false;
 	}
-
-//	/**
-//     * main de la clase.
-//     * @param args Se ignoran
-//     */
-//    public static void main(String[] args)
-//    {
-//      // Se obtiene la Session
-//        Properties prop = new Properties();
-//        prop.setProperty("mail.pop3.starttls.enable", "false");
-//        prop.setProperty("mail.pop3.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-//        prop.setProperty("mail.pop3.socketFactory.fallback", "false");
-//        prop.setProperty("mail.pop3.port", "995");
-//        prop.setProperty("mail.pop3.socketFactory.port", "995");
-//        Session sesion = Session.getInstance(prop);
-//        // sesion.setDebug(true);
-//
-//        try
-//        {
-//          // Se obtiene el Store y el Folder, para poder leer el
-//          // correo.
-//            Store store = sesion.getStore("pop3");
-//            store.connect("pop.gmail.com", "nicolasarielmartini@gmail.com", "n1c0l4589");
-//            Folder folder = store.getFolder("INBOX");
-//            folder.open(Folder.READ_ONLY);
-//
-//            // Se obtienen los mensajes.
-//            Message[] mensajes = folder.getMessages();
-//
-//            // Se escribe from y subject de cada mensaje
-//            for (int i = 0; i < mensajes.length; i++)
-//            {
-//                System.out.println(
-//                    "From:" + mensajes[i].getFrom()[0].toString());
-//                System.out.println("Subject:" + mensajes[i].getSubject());
-//                
-//                // Se visualiza, si se sabe como, el contenido de cada mensaje
-//                //analizaParteDeMensaje(mensajes[i]);
-//            }
-//
-//            folder.close(false);
-//            store.close();
-//        }
-//        catch (Exception e)
-//        {
-//            e.printStackTrace();
-//        }
-//    }
-
-//    /**
-//     * Metodo recursivo.
-//     * Si la parte que se pasa es compuesta, se extrae cada una de las subpartes y
-//     * el metodo se llama a si mismo con cada una de ellas.
-//     * Si la parte es un text, se escribe en pantalla.
-//     * Si la parte es una image, se guarda en un fichero y se visualiza en un JFrame.
-//     * En cualquier otro caso, simplemente se escribe el tipo recibido, pero se
-//     * ignora el mensaje.
-//     *
-//     * @param unaParte Parte del mensaje a analizar.
-//     */
-//    private static void analizaParteDeMensaje(Part unaParte)
-//    {
-//        try
-//        {
-//          // Si es multipart, se analiza cada una de sus partes recursivamente.
-//            if (unaParte.isMimeType("multipart/*"))
-//            {
-//                Multipart multi;
-//                multi = (Multipart) unaParte.getContent();
-//
-//                for (int j = 0; j < multi.getCount(); j++)
-//                {
-//                    analizaParteDeMensaje(multi.getBodyPart(j));
-//                }
-//            }
-//            else
-//            {
-//              // Si es texto, se escribe el texto.
-//                if (unaParte.isMimeType("text/*"))
-//                {
-//                    System.out.println("Texto " + unaParte.getContentType());
-//                    System.out.println(unaParte.getContent());
-//                    System.out.println("---------------------------------");
-//                }
-//                else
-//                {
-//                  // Si es imagen, se guarda en fichero y se visualiza en JFrame
-//                    if (unaParte.isMimeType("image/*"))
-//                    {
-//                        System.out.println(
-//                            "Imagen " + unaParte.getContentType());
-//                        System.out.println("Fichero=" + unaParte.getFileName());
-//                        System.out.println("---------------------------------");
-//
-//                        salvaImagenEnFichero(unaParte);
-//                        visualizaImagenEnJFrame(unaParte);
-//                    }
-//                    else
-//                    {
-//                      // Si no es ninguna de las anteriores, se escribe en pantalla
-//                      // el tipo.
-//                        System.out.println(
-//                            "Recibido " + unaParte.getContentType());
-//                        System.out.println("---------------------------------");
-//                    }
-//                }
-//            }
-//        }
-//        catch (Exception e)
-//        {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    /**
-//     * Presupone que unaParte es una foto adjunta a un correo.
-//     * Recoge la imagen y la visualiza en un JFrame
-//     *
-//     * @param unaParte Parte de un correo correspondiente a una imagen.
-//     *
-//     * @throws IOException 
-//     * @throws MessagingException 
-//     */
-//    private static void visualizaImagenEnJFrame(Part unaParte)
-//        throws IOException, MessagingException
-//    {
-//        JFrame v = new JFrame();
-//        ImageIcon icono = new ImageIcon(
-//                ImageIO.read(unaParte.getInputStream()));
-//        JLabel l = new JLabel(icono);
-//        v.getContentPane().add(l);
-//        v.pack();
-//        v.setVisible(true);
-//    }
-//
-//    /**
-//     * Supone que unaParte corresponde a una imagen de un fichero y que
-//     * getFileName() esta relleno.
-//     * Salva la imagen en d:\getFileName().
-//     *
-//     * @param unaParte Parte de un correo correspondiente a una imagen.
-//     *
-//     * @throws FileNotFoundException 
-//     * @throws MessagingException 
-//     * @throws IOException 
-//     */
-//    private static void salvaImagenEnFichero(Part unaParte)
-//        throws FileNotFoundException, MessagingException, IOException
-//    {
-//        FileOutputStream fichero = new FileOutputStream(
-//                "d:/" + unaParte.getFileName());
-//        InputStream imagen = unaParte.getInputStream();
-//        byte[] bytes = new byte[1000];
-//        int leidos = 0;
-//
-//        while ((leidos = imagen.read(bytes)) > 0)
-//        {
-//            fichero.write(bytes, 0, leidos);
-//        }
-//    }
-//
-//	
-////	public static void main(String []args) {
-////		
-////		principal p = new principal();
-////		p.conectar();
-////		
-////	}
-	
 
 }
